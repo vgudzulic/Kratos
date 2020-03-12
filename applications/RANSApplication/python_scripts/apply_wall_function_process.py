117314f68f36d8a6a677e53020d0f8564610d28e
sunethwarna <suneth.warna@gmail.com>
2020-03-12 17:34:11 +0100

    update wall function

--- applications/RANSApplication/python_scripts/apply_wall_function_process.py
+++ applications/RANSApplication/python_scripts/apply_wall_function_process.py
@@ -19,12 +19,23 @@ class ApplyWallFunctionProcess(KratosMultiphysics.Process):
         settings.ValidateAndAssignDefaults(default_parameters)
 
         self.model_part = Model[settings["model_part_name"].GetString()]
+        self.domain_size = self.model_part.ProcessInfo[KratosMultiphysics.DOMAIN_SIZE]
+
+    def ExecuteInitialize(self):
+        # Compute the normal on the nodes of interest
+        KratosMultiphysics.NormalCalculationUtils().CalculateOnSimplex(self.model_part, self.domain_size)
+
+        for node in self.model_part.Nodes:
+            node.Set(KratosMultiphysics.SLIP, True)
+            node.Set(KratosMultiphysics.STRUCTURE, True)
+            node.SetSolutionStepValue(KratosMultiphysics.MESH_VELOCITY,0,[0.0, 0.0, 0.0])
 
         # Mark the nodes and conditions with the appropriate slip flag
         for condition in self.model_part.Conditions:
             condition.Set(KratosMultiphysics.SLIP, True)
             condition.Set(KratosMultiphysics.STRUCTURE, True)
 
-        for node in self.model_part.Nodes:
-            node.Set(KratosMultiphysics.SLIP, True)
-            node.Set(KratosMultiphysics.STRUCTURE, True)
+    def ExecuteInitializeSolutionStep(self):
+        KratosMultiphysics.VariableUtils().SetFlag(KratosMultiphysics.SLIP, False, self.model_part.Nodes)
+        KratosMultiphysics.VariableUtils().SetVectorVar(KratosMultiphysics.MESH_VELOCITY, [0.0, 0.0, 0.0], self.model_part.Nodes)
+
