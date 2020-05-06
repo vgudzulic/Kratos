549f6a7ca41847d847b1463710c386c33952285d
Unknown <apuigferrat@cimne.upc.edu>
2020-05-06 17:07:37 +0200

    Updating manufactured solution script

--- applications/FluidTransportApplication/python_scripts/apply_scalar_manufactured_constraint_function_process.py
+++ applications/FluidTransportApplication/python_scripts/apply_scalar_manufactured_constraint_function_process.py
@@ -2,8 +2,9 @@ import KratosMultiphysics
 import KratosMultiphysics.FluidTransportApplication as KratosFluidTransport
 
 from math import pi
-from sympy import Symbol, diff, lambdify, sin, atan, exp
-from sympy.vector import CoordSysCartesian, gradient, divergence #, laplacian
+from sympy import Symbol, diff, lambdify, sin, cos, atan, exp
+from sympy.vector import CoordSys3D, gradient, divergence #, laplacian
+import numpy as np
 
 def Factory(settings, Model):
     if(type(settings) != KratosMultiphysics.Parameters):
@@ -20,24 +21,13 @@ class ApplyScalarManufacturedConstraintFunctionProcess(KratosMultiphysics.Proces
 
         # self.a1 = 2
         # self.a2 = 3
-        # self.k = 0.000001
-        # self.s = 1000
+        k = 0.000001
+        s = 20
 
-        c = CoordSysCartesian('c')
-        t = Symbol('t')
-
-        # self.a = a1*c.i + a2*c.j
-        # self.u = 16 * sympy.sin(pi * self.t) * c.x(1 - c.x) * c.y(1 - c.y) * (1/2 + (sympy.atan(2 * self.k ** (-1/2) * (0.25**2 - (c.x - 0.5)**2 - (c.y - 0.5)**2)))/pi)
-        #  self.u = (c.x**2.0/2/a1 + k*c.x/a1**2 + (1.0/2/a1 + k/a1**2) * (exp(-a1/k) - exp(-a1/k*(1-c.x))) / (1 - exp(-a1/k)))
-        a, u = self._GenerateAnalyticFields(settings, c, t)
-
-        f = diff(u, t) - k*divergence(gradient(u, c), c) + a.dot(gradient(u, c)) + s*u
-
-        # Converting the symbolic fields to numeric functions
-        self.velocity = lambdify([c.x, c.y, c.z, t], [a.dot(c.i), a.dot(c.j), a.dot(c.k)], 'numpy')
-        self.analytic_phi = lambdify([c.x, c.y, c.z, t], u, 'numpy')
-        self.source_term = lambdify([c.x, c.y, c.z, t], f, 'numpy')
-
+        time = self.model_part.ProcessInfo[KratosMultiphysics.TIME]
+        for node in self.model_part.Nodes:
+            f = 4.0*node.X*node.Y*k**(-0.5)*(1.0 - 2*node.X)*(1 - node.Y)*(16 - 16*node.X)*sin(pi*time)/(pi*(4*k**(-1.0)*(-(node.X - 0.5)**2 - (node.Y - 0.5)**2 + 0.0625)**2 + 1)) + 6.0*node.X*node.Y*k**(-0.5)*(1.0 - 2*node.Y)*(1 - node.Y)*(16 - 16*node.X)*sin(pi*time)/(pi*(4*k**(-1.0)*(-(node.X - 0.5)**2 - (node.Y - 0.5)**2 + 0.0625)**2 + 1)) + 16*node.X*node.Y*s*(1 - node.X)*(1 - node.Y)*(atan(2*k**(-0.5)*(-(node.X - 0.5)**2 - (node.Y - 0.5)**2 + 0.0625))/pi + 0.5)*sin(pi*time) + node.X*node.Y*pi*(1 - node.Y)*(16 - 16*node.X)*(atan(2*k**(-0.5)*(-(node.X - 0.5)**2 - (node.Y - 0.5)**2 + 0.0625))/pi + 0.5)*cos(pi*time) + 3.0*node.X*node.Y*(16*node.X - 16)*(atan(2*k**(-0.5)*(-(node.X - 0.5)**2 - (node.Y - 0.5)**2 + 0.0625))/pi + 0.5)*sin(pi*time) + 2.0*node.X*node.Y*(16*node.Y - 16)*(atan(2*k**(-0.5)*(-(node.X - 0.5)**2 - (node.Y - 0.5)**2 + 0.0625))/pi + 0.5)*sin(pi*time) + 3.0*node.X*(1 - node.Y)*(16 - 16*node.X)*(atan(2*k**(-0.5)*(-(node.X - 0.5)**2 - (node.Y - 0.5)**2 + 0.0625))/pi + 0.5)*sin(pi*time) + 2.0*node.Y*(1 - node.Y)*(16 - 16*node.X)*(atan(2*k**(-0.5)*(-(node.X - 0.5)**2 - (node.Y - 0.5)**2 + 0.0625))/pi + 0.5)*sin(pi*time) - k*(-8*node.X*node.Y*k**(-1.5)*(1.0 - 2*node.X)*(1 - node.Y)*(2.0 - 4*node.X)*(16 - 16*node.X)*(-(node.X - 0.5)**2 - (node.Y - 0.5)**2 + 0.0625)*sin(pi*time)/(pi*(4*k**(-1.0)*(-(node.X - 0.5)**2 - (node.Y - 0.5)**2 + 0.0625)**2 + 1)**2) - 8*node.X*node.Y*k**(-1.5)*(1.0 - 2*node.Y)*(1 - node.Y)*(2.0 - 4*node.Y)*(16 - 16*node.X)*(-(node.X - 0.5)**2 - (node.Y - 0.5)**2 + 0.0625)*sin(pi*time)/(pi*(4*k**(-1.0)*(-(node.X - 0.5)**2 - (node.Y - 0.5)**2 + 0.0625)**2 + 1)**2) - 32*node.X*node.Y*k**(-0.5)*(1.0 - 2*node.X)*(1 - node.Y)*sin(pi*time)/(pi*(4*k**(-1.0)*(-(node.X - 0.5)**2 - (node.Y - 0.5)**2 + 0.0625)**2 + 1)) + 2*node.X*node.Y*k**(-0.5)*(1.0 - 2*node.X)*(16*node.Y - 16)*sin(pi*time)/(pi*(4*k**(-1.0)*(-(node.X - 0.5)**2 - (node.Y - 0.5)**2 + 0.0625)**2 + 1)) - 2*node.X*node.Y*k**(-0.5)*(1.0 - 2*node.Y)*(16 - 16*node.X)*sin(pi*time)/(pi*(4*k**(-1.0)*(-(node.X - 0.5)**2 - (node.Y - 0.5)**2 + 0.0625)**2 + 1)) + 2*node.X*node.Y*k**(-0.5)*(1.0 - 2*node.Y)*(16*node.X - 16)*sin(pi*time)/(pi*(4*k**(-1.0)*(-(node.X - 0.5)**2 - (node.Y - 0.5)**2 + 0.0625)**2 + 1)) - 8*node.X*node.Y*k**(-0.5)*(1 - node.Y)*(16 - 16*node.X)*sin(pi*time)/(pi*(4*k**(-1.0)*(-(node.X - 0.5)**2 - (node.Y - 0.5)**2 + 0.0625)**2 + 1)) + 4*node.X*k**(-0.5)*(1.0 - 2*node.Y)*(1 - node.Y)*(16 - 16*node.X)*sin(pi*time)/(pi*(4*k**(-1.0)*(-(node.X - 0.5)**2 - (node.Y - 0.5)**2 + 0.0625)**2 + 1)) + 2*node.X*(16*node.X - 16)*(atan(2*k**(-0.5)*(-(node.X - 0.5)**2 - (node.Y - 0.5)**2 + 0.0625))/pi + 0.5)*sin(pi*time) + 4*node.Y*k**(-0.5)*(1.0 - 2*node.X)*(1 - node.Y)*(16 - 16*node.X)*sin(pi*time)/(pi*(4*k**(-1.0)*(-(node.X - 0.5)**2 - (node.Y - 0.5)**2 + 0.0625)**2 + 1)) + 2*node.Y*(16*node.Y - 16)*(atan(2*k**(-0.5)*(-(node.X - 0.5)**2 - (node.Y - 0.5)**2 + 0.0625))/pi + 0.5)*sin(pi*time))
+            node.SetSolutionStepValue(KratosMultiphysics.HEAT_FLUX,f)
         # self.components_process_list = []
 
         # for node in model_part.Nodes:
@@ -61,27 +51,30 @@ class ApplyScalarManufacturedConstraintFunctionProcess(KratosMultiphysics.Proces
 
 
 
-    def ExecuteInitializeSolutionStep(self):
-        time = self.model_part.ProcessInfo[TIME]
-        for node in self.model_part.Nodes:
-            f = self.source_term(node.X, node.Y, node.Z, time)
-            a = self.velocity(node.X, node.Y, node.Z, time)
-            node.FastGetSolutionStepValue(SOURCE_TERM) = f
-            node.FastGetSolutionStepValue(VELOCITY) = a
+    # def ExecuteInitializeSolutionStep(self):
+    #     time = self.model_part.ProcessInfo[KratosMultiphysics.TIME]
+    #     for node in self.model_part.Nodes:
+    #         f = self.source_term(node.X, node.Y, node.Z, time)
+    #         a = self.velocity(node.X, node.Y, node.Z, time)
+    #         node.SetSolutionStepValue(KratosMultiphysics.HEAT_FLUX,f)
+    #         # node.FastGetSolutionStepValue(HEAT_FLUX) = f
+    #        # node.FastGetSolutionStepValue(VELOCITY) = a
 
-    def ExecuteFinalizeSolutionStep(self):
-        time = self.model_part.ProcessInfo[TIME]
-        for node in self.model_part.Nodes:
-            u = self.analytic_phi(node.X, node.Y, node.Z, time)
-            node.FastGetSolutionStepValue(ANALYTIC_PHI) = u
+    # # def ExecuteFinalizeSolutionStep(self):
+    # #     time = self.model_part.ProcessInfo[KratosMultiphysics.TIME]
+    # #     for node in self.model_part.Nodes:
+    # #         u = self.analytic_phi(node.X, node.Y, node.Z, time)
+    # #         node.FastGetSolutionStepValue(ANALYTIC_PHI) = u
 
-    def _GenerateAnalyticFields(self, settings, c, t):
-        a1 = settings['velocity'][0].GetDouble()
-        a2 = settings['velocity'][1].GetDouble()
-        k = settings['diffusivity'].GetDouble()
-        s = settings['absorption'].GetDouble()
+    # def _GenerateAnalyticFields(self, settings, c, t):
+    #     # a1 = settings['velocity'][0].GetDouble()
+    #     # a2 = settings['velocity'][1].GetDouble()
+    #     # k = settings['diffusivity'].GetDouble()
+    #     # s = settings['absorption'].GetDouble()
+    #     k = 0.000001
+    #     s = 1000
+    #     # a = a1*c.i + a2*c.j
+    #     a =[2.0, 3.0]
+    #     u = 16 * sin(pi * t) * c.x*(1 - c.x) * c.y*(1 - c.y) * (1/2 + (atan(2 * k ** (-1/2) * (0.25**2 - (c.x - 0.5)**2 - (c.y - 0.5)**2)))/pi)
 
-        a = a1*c.i + a2*c.j
-        u = 16 * sin(pi * t) * c.x(1 - c.x) * c.y(1 - c.y) * (1/2 + (atan(2 * k ** (-1/2) * (0.25**2 - (c.x - 0.5)**2 - (c.y - 0.5)**2)))/pi)
-
-        return a, u
+    #     return a, u
