91b8679b27d03e7d113d6618c1643116160da09f
Ignasi de Pouplana <ipouplana@cimne.upc.edu>
2020-07-13 18:03:17 +0200

    First additions for split FE in DEM

---
+++ applications/DEMApplication/custom_strategies/schemes/split_forward_euler_scheme.h
@@ -0,0 +1,178 @@
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
+//    . \  |   (   | |   (   |\__ `
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
+//
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
+//
+//  Main authors:    Ignasi de Pouplana
+//
+//
+
+#if !defined(KRATOS_SPLIT_FORWARD_EULER_SCHEME_H_INCLUDED )
+#define  KRATOS_SPLIT_FORWARD_EULER_SCHEME_H_INCLUDED
+
+// System includes
+#include <string>
+#include <iostream>
+#include <cfloat>
+
+// Project includes
+#include "dem_integration_scheme.h"
+#include "includes/define.h"
+#include "utilities/openmp_utils.h"
+#include "includes/model_part.h"
+#include "custom_utilities/GeometryFunctions.h"
+#include "utilities/quaternion.h"
+
+namespace Kratos {
+
+    class KRATOS_API(DEM_APPLICATION) SplitForwardEulerScheme : public DEMIntegrationScheme {
+    public:
+
+        typedef ModelPart::NodesContainerType NodesArrayType;
+
+        /// Pointer definition of SplitForwardEulerScheme
+        KRATOS_CLASS_POINTER_DEFINITION(SplitForwardEulerScheme);
+
+        /// Default constructor.
+        SplitForwardEulerScheme() {}
+
+        /// Destructor.
+        virtual ~SplitForwardEulerScheme() {}
+
+        DEMIntegrationScheme* CloneRaw() const override {
+            DEMIntegrationScheme* cloned_scheme(new SplitForwardEulerScheme(*this));
+            return cloned_scheme;
+        }
+
+        DEMIntegrationScheme::Pointer CloneShared() const override {
+            DEMIntegrationScheme::Pointer cloned_scheme(new SplitForwardEulerScheme(*this));
+            return cloned_scheme;
+        }
+
+        void SetTranslationalIntegrationSchemeInProperties(Properties::Pointer pProp, bool verbose = true) const override;
+        void SetRotationalIntegrationSchemeInProperties(Properties::Pointer pProp, bool verbose = true) const override;
+
+        void UpdateTranslationalVariables(
+                int StepFlag,
+                Node < 3 >& i,
+                array_1d<double, 3 >& coor,
+                array_1d<double, 3 >& displ,
+                array_1d<double, 3 >& delta_displ,
+                array_1d<double, 3 >& vel,
+                const array_1d<double, 3 >& initial_coor,
+                const array_1d<double, 3 >& force,
+                const double force_reduction_factor,
+                const double mass,
+                const double delta_t,
+                const bool Fix_vel[3]) override;
+
+        void CalculateNewRotationalVariablesOfSpheres(
+                int StepFlag,
+                Node < 3 >& i,
+                const double moment_of_inertia,
+                array_1d<double, 3 >& angular_velocity,
+                array_1d<double, 3 >& torque,
+                const double moment_reduction_factor,
+                array_1d<double, 3 >& rotated_angle,
+                array_1d<double, 3 >& delta_rotation,
+                const double delta_t,
+                const bool Fix_Ang_vel[3]) override;
+
+        void CalculateNewRotationalVariablesOfRigidBodyElements(
+                int StepFlag,
+                Node < 3 >& i,
+                const array_1d<double, 3 > moments_of_inertia,
+                array_1d<double, 3 >& angular_velocity,
+                array_1d<double, 3 >& torque,
+                const double moment_reduction_factor,
+                array_1d<double, 3 >& rotated_angle,
+                array_1d<double, 3 >& delta_rotation,
+                Quaternion<double  >& Orientation,
+                const double delta_t,
+                const bool Fix_Ang_vel[3]) override;
+
+        void UpdateRotationalVariables(
+                int StepFlag,
+                Node < 3 >& i,
+                array_1d<double, 3 >& rotated_angle,
+                array_1d<double, 3 >& delta_rotation,
+                array_1d<double, 3 >& angular_velocity,
+                array_1d<double, 3 >& angular_acceleration,
+                const double delta_t,
+                const bool Fix_Ang_vel[3]) override;
+
+        void CalculateLocalAngularAcceleration(
+                const double moment_of_inertia,
+                const array_1d<double, 3 >& torque,
+                const double moment_reduction_factor,
+                array_1d<double, 3 >& angular_acceleration) override;
+
+        void CalculateLocalAngularAccelerationByEulerEquations(
+                const array_1d<double, 3 >& local_angular_velocity,
+                const array_1d<double, 3 >& moments_of_inertia,
+                const array_1d<double, 3 >& local_torque,
+                const double moment_reduction_factor,
+                array_1d<double, 3 >& local_angular_acceleration) override;
+
+        /// Turn back information as a string.
+
+        virtual std::string Info() const override {
+            std::stringstream buffer;
+            buffer << "SplitForwardEulerScheme";
+            return buffer.str();
+        }
+
+        /// Print information about this object.
+
+        virtual void PrintInfo(std::ostream& rOStream) const override {
+            rOStream << "SplitForwardEulerScheme";
+        }
+
+        /// Print object's data.
+
+        virtual void PrintData(std::ostream& rOStream) const override {
+        }
+
+
+    protected:
+
+
+    private:
+
+    /// Assignment operator.
+
+        SplitForwardEulerScheme& operator=(SplitForwardEulerScheme const& rOther) {
+            return *this;
+        }
+
+        /// Copy constructor.
+
+        SplitForwardEulerScheme(SplitForwardEulerScheme const& rOther) {
+            *this = rOther;
+        }
+
+        ///@}
+
+    }; // Class SplitForwardEulerScheme
+
+    inline std::istream& operator>>(std::istream& rIStream,
+            SplitForwardEulerScheme& rThis) {
+        return rIStream;
+    }
+
+    inline std::ostream& operator<<(std::ostream& rOStream,
+            const SplitForwardEulerScheme& rThis) {
+        rThis.PrintInfo(rOStream);
+        rOStream << std::endl;
+        rThis.PrintData(rOStream);
+
+        return rOStream;
+    }
+
+} // namespace Kratos.
+
+#endif // KRATOS_SPLIT_FORWARD_EULER_SCHEME_H_INCLUDED  defined
